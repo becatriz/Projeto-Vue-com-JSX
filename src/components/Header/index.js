01e141dce2a72d846e660eaaa1f3527404bd6176
Rebeca Lopes <rebeca.cruz@before.com.br>
2021-05-17 22:58:35 -0300

    feat: cria componente Header

---
+++ src/components/Header/index.js
@@ -0,0 +1,17 @@
+import "./_styles.scss";
+
+export const Header = {
+  name: "Header",
+
+  render() {
+    return (
+      <header>
+        <nav>
+          <div class="logo">Logo</div>
+        </nav>
+      </header>
+    );
+  },
+};
+
+export default Header;
