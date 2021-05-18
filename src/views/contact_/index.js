78d08670edbb76b7f850a23f5eee017c720a164f
Rebeca Lopes <rebeca.cruz@before.com.br>
2021-05-17 22:59:30 -0300

    feat: cria paginas Home e Contact

---
+++ src/views/contact_/index.js
@@ -0,0 +1,7 @@
+export default {
+  name: "Contact",
+
+  render() {
+    return <h1>Contato</h1>;
+  },
+};
---
+++ src/views/home_/index.js
@@ -0,0 +1,11 @@
+export default {
+  name: "Home",
+
+  render() {
+    return (
+      <div>
+        <h1>Home</h1>
+      </div>
+    );
+  },
+};
